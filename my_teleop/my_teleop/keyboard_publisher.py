a4b1e499d274988fb8d97c7ae7da2a7037c221f8
Yassine <yas3tech@gmail.com>
2025-12-05 00:03:07 +0000

    add keyboard package

---
+++ my_teleop/my_teleop/keyboard_publisher.py
@@ -0,0 +1,27 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class KeyboardPublisher(Node):
+    def __init__(self):
+        super().__init__('keyboard_publisher')
+        self.pub = self.create_publisher(String, 'key_cmd', 10)
+        print("z=forward, s=backward, a=left, e=right, q=quit")
+
+    def run(self):
+        while rclpy.ok():
+            key = input("> ")
+            if key == "q":
+                break
+            msg = String()
+            msg.data = key
+            self.pub.publish(msg)
+
+def main():
+    rclpy.init()
+    node = KeyboardPublisher()
+    node.run()
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
