377f123d0d4ae8086b8ac231e1a8b8ecc1adb8ba
Yassine <yas3tech@gmail.com>
2025-12-05 00:40:42 +0000

    Add velocity teleop

--- my_teleop/my_teleop/cmd_subscriber.py
+++ my_teleop/my_teleop/cmd_subscriber.py
@@ -6,26 +6,48 @@ import serial
 class CmdSubscriber(Node):
     def __init__(self):
         super().__init__('cmd_subscriber')
-        # Port série de l'OpenCR
+
+
         self.ser = serial.Serial('/dev/ttyACM0', 57600, timeout=0.1)
-        # Abonnement au topic venant du PC
+
+
+        self.left_speed = 0
+        self.right_speed = 0
+
         self.create_subscription(String, 'key_cmd', self.callback, 10)
+
+    def send_velocity(self):
+        cmd = f"V {self.left_speed} {self.right_speed}\n"
+        self.ser.write(cmd.encode())
+        print("send:", cmd.strip())
 
     def callback(self, msg):
         key = msg.data
 
         if key == 'z':
-            cmd = "D 50 50 1\n"       # avant
-        elif key == 's':
-            cmd = "D -50 -50 1\n"     # arrière
-        elif key == 'a':
-            cmd = "D 50 -50 1\n"      # gauche
-        elif key == 'e':
-            cmd = "D -50 50 1\n"      # droite
-        else:
-            return  # touche inconnue → on ne fait rien
+            self.left_speed += 5
+            self.right_speed += 5
 
-        self.ser.write(cmd.encode())
+        elif key == 's':
+
+            self.left_speed -= 5
+            self.right_speed -= 5
+
+        elif key == 'a':
+
+            self.left_speed += 5
+            self.right_speed -= 5
+
+        elif key == 'x':
+
+            self.left_speed = 0
+            self.right_speed = 0
+
+        else:
+
+            return
+
+        self.send_velocity()
 
 def main():
     rclpy.init()
