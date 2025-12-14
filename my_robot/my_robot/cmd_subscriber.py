89dbbfcd064badc0a531e05ceed2696587e89592
Yassine <yas3tech@gmail.com>
2025-12-14 19:35:31 +0000

    Update cmd_sub

--- my_robot/my_robot/cmd_subscriber.py
+++ my_robot/my_robot/cmd_subscriber.py
@@ -3,29 +3,25 @@ from rclpy.node import Node
 from std_msgs.msg import String
 import serial
 
+KEYMAP = {
+    'z': "D 50 50 1",
+    'a': "D 50 -50 1",
+    'e': "D -50 50 1",
+    's': "D -50 -50 1",
+}
+
 class CmdSubscriber(Node):
     def __init__(self):
         super().__init__('cmd_subscriber')
         self.ser = serial.Serial('/dev/ttyACM0', 57600, timeout=0.1)
-        self.create_subscription(String, 'key_cmd', self.callback, 10)
+        self.create_subscription(String, 'key_cmd', self.cb, 10)
 
-    def callback(self, msg):
+    def cb(self, msg):
         key = msg.data
-
-        if key == 'z':        # forward
-            cmd = "V -50 -50\n"
-        elif key == 's':      # backward
-            cmd = "V 50 50\n"
-        elif key == 'a':      # left
-            cmd = "V 50 -50\n"
-        elif key == 'e':      # right
-            cmd = "V -50 50\n"
-        elif key == 'x':      # stop
-            cmd = "V 0 0\n"
-        else:
-            return
-
-        self.ser.write(cmd.encode())
+        if key in KEYMAP:
+            cmd = KEYMAP[key]
+            self.ser.write((cmd + "\n").encode())
+            self.get_logger().info(f"> {cmd}")
 
 def main():
     rclpy.init()
