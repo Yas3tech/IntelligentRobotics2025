dcfbdb44df4679fc801555e051c99c7e882b8149
Yassine <yas3tech@gmail.com>
2025-12-14 20:21:22 +0000

    Update cmd_sub

--- my_robot/my_robot/cmd_subscriber.py
+++ my_robot/my_robot/cmd_subscriber.py
@@ -1,34 +1,48 @@
+import sys
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
-import serial
 
-KEYMAP = {
-    'z': "D 50 50 1",
-    'a': "D 50 -50 1",
-    'e': "D -50 50 1",
-    's': "D -50 -50 1",
-}
 
-class CmdSubscriber(Node):
+class KeyboardPublisher(Node):
     def __init__(self):
-        super().__init__('cmd_subscriber')
-        self.ser = serial.Serial('/dev/ttyACM0', 57600, timeout=0.1)
-        self.create_subscription(String, 'key_cmd', self.cb, 10)
+        super().__init__('keyboard_publisher')
+        self.pub = self.create_publisher(String, 'key_cmd', 10)
+        print("z=forward | s=backward | a=left | e=right | q=quit")
 
-    def cb(self, msg):
-        key = msg.data
-        if key in KEYMAP:
-            cmd = KEYMAP[key]
-            self.ser.write((cmd + "\n").encode())
-            self.get_logger().info(f"> {cmd}")
+    def run(self):
+        while rclpy.ok():
+            try:
+                key = input("> ")
+            except UnicodeDecodeError:
+                continue
 
-def main():
-    rclpy.init()
-    node = CmdSubscriber()
-    rclpy.spin(node)
+            key = key.strip()
+            if not key:
+                continue
+
+            key = key[0]
+
+            if key == "q":
+                break
+
+            msg = String()
+            msg.data = key
+            self.pub.publish(msg)
+
+
+def main(args=None):
+    try:
+        sys.stdin.reconfigure(encoding="utf-8", errors="ignore")
+    except Exception:
+        pass
+
+    rclpy.init(args=args)
+    node = KeyboardPublisher()
+    node.run()
+    node.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == "__main__":
     main()
-
